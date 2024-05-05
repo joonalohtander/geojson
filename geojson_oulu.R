92173435b725187abbbf5eb6606c146525fabfa1
joona <joona@debian.debian>
2024-05-05 10:12:30 +0300

    added oulu

--- geojson_oulu.R
+++ geojson_oulu.R
@@ -2,6 +2,7 @@ library(geofi)
 library(data.table)
 library(sf)
 library(tidyverse)
+library(here)
 i_am("geojson.R")
 
 # postal code areas
@@ -10,20 +11,10 @@ geoms <- get_zipcodes(year = 2024, extend_to_sea_areas = F) %>%
   as.data.table() %>% 
   .[, .(postal_code = posti_alue,
         area = nimi,
-        geom)]
+        geom,
+        kuntanro)] %>% 
+  .[kuntanro == 564]
 
 geoms %>% 
-  st_write("posti.geojson", 
-           delete_dsn = T)
-
-# municipalities
-geoms <- get_municipalities(year = 2024) %>% 
-  st_transform(4326) %>% 
-  as.data.table() %>% 
-  .[, .(municipality = name,
-        municipality_key = kunta,
-        geom)]
-
-geoms %>% 
-  st_write("kunta.geojson", 
+  st_write(here("./oulu/posti.geojson"), 
            delete_dsn = T)
